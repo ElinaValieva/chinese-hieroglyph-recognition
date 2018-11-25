0e424f7cc4863da2fe126b9f77060df2f809be3c
ElinaValieva <veaufa@mail.ru>
2018-11-25 18:07:43 +0300

    chore: saving file in project directory

--- src/main/java/MLP/util/FileUtil.java
+++ src/main/java/MLP/util/FileUtil.java
@@ -3,9 +3,15 @@ package MLP.util;
 import lombok.extern.log4j.Log4j;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Log4j
 public class FileUtil {
+
+    private static final String USER_DIR = "user.dir";
 
     private FileUtil() {
     }
@@ -13,5 +19,10 @@ public class FileUtil {
     public static String getFilesPath(String fileName) {
         File file = new File(FileUtil.class.getClassLoader().getResource(fileName).getFile());
         return file.getAbsolutePath();
+    }
+
+    public static File createFile(String fileName) throws IOException {
+        Path path = Paths.get(System.getProperty(USER_DIR) + fileName);
+        return Files.createFile(path).toFile();
     }
 }
