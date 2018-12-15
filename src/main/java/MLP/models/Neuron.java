63b35346354cadfda7597d658079e730fa2a6e07
ElinaValieva <veaufa@mail.ru>
2018-12-15 14:24:59 +0300

    fix: models for MLP

--- src/main/java/MLP/models/Neuron.java
+++ src/main/java/MLP/models/Neuron.java
@@ -2,19 +2,21 @@ package MLP.models;
 import java.util.stream.IntStream;
 
 public class Neuron {
+
     public double value;
     public double[] weights;
     public double bias;
     public double delta;
+    public static final double DELTA = 10000000000000.0;
 
     public Neuron(int prevLayerSize) {
         weights = new double[prevLayerSize];
-        bias = Math.random() / 10000000000000.0;
-        delta = Math.random() / 10000000000000.0;
-        value = Math.random() / 10000000000000.0;
+        bias = Math.random() / DELTA;
+        delta = Math.random() / DELTA;
+        value = Math.random() / DELTA;
 
         IntStream.range(0, weights.length)
                 .forEach(i ->
-                        weights[i] = Math.random() / 10000000000000.0);
+                        weights[i] = Math.random() / DELTA);
     }
 }
--- src/main/java/MLP/models/RImage.java
+++ src/main/java/MLP/models/RImage.java
@@ -1,25 +1,21 @@
 package MLP.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 @Setter
+@AllArgsConstructor
 @NoArgsConstructor
 public class RImage implements Cloneable {
 
     private int sizeX;
     private int sizeY;
     private List<Integer> pixels;
-
-    public void refresh() {
-        sizeX = sizeY = 0;
-        pixels = new ArrayList<>();
-    }
 
     public RImage clone() throws CloneNotSupportedException {
         return (RImage) super.clone();
