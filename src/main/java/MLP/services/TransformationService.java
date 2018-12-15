19cb4d7958f4a92802e39e1e04ee39633dd74a03
ElinaValieva <veaufa@mail.ru>
2018-12-15 17:29:42 +0300

    fix: transformationService using fileManagerService

--- src/main/java/MLP/services/TransformationService.java
+++ src/main/java/MLP/services/TransformationService.java
@@ -5,9 +5,7 @@ import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
@@ -23,22 +21,29 @@ public class TransformationService {
     private ISegmentationService segmentationService;
 
     @Autowired
-    private IFileService fileService;
+    private IResourcesService fileService;
+
+    @Autowired
+    private IFileManagerService fileManagerService;
 
     public void transform() {
-        RImage rImage = fileService.createRImage(fileService.getFilesPath("tests_Hieroglyph/8.png"), 50, 50);
-        List<RImage> rImageList = segmentationService.segmentation(rImage);
-        final int[] index = {0};
-        rImageList.forEach(rImageItem -> {
-            RImage newRImage = fileService.resizeImage(rImageItem);
-            BufferedImage bufferedImage = fileService.convertToImage(newRImage);
-            String fileName = "C:/Users/Elina/Desktop/file" + index[0] + ".png";
-            try {
-                ImageIO.write(bufferedImage, "png", new File(fileName));
-            } catch (IOException e) {
-                log.error("", e);
-            }
-            index[0]++;
-        });
+        try {
+            fileManagerService.deleteAll();
+            RImage rImage = fileService.createRImage(fileService.getFilesPath("tests_Hieroglyph/8.png"), 50, 50);
+            List<RImage> rImageList = segmentationService.segmentation(rImage);
+            rImageList.forEach(rImageItem -> {
+                try {
+                    RImage newRImage = fileService.resizeImage(rImageItem);
+                    BufferedImage bufferedImage = fileService.convertToImage(newRImage);
+                    String fileName = "file" + rImageList.indexOf(rImageItem) + ".png";
+                    System.out.println(fileName);
+                    fileManagerService.saveFile(bufferedImage, fileName);
+                } catch (IOException e) {
+                    log.error("", e);
+                }
+            });
+        } catch (IOException e) {
+            log.error("", e);
+        }
     }
 }
