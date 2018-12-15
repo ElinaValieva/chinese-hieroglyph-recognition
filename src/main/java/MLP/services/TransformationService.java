7d16e2c702975746e1c979b4b728a7812d25bcd3
ElinaValieva <veaufa@mail.ru>
2018-12-15 20:27:50 +0300

    chore: testing and training features

--- src/main/java/MLP/services/TransformationService.java
+++ src/main/java/MLP/services/TransformationService.java
@@ -1,6 +1,11 @@
 package MLP.services;
 
+import MLP.funtionsActivation.SigmoidActivation;
 import MLP.models.RImage;
+import MLP.services.api.IFileManagerService;
+import MLP.services.api.IMultiLayerPerceptronService;
+import MLP.services.api.IResourcesService;
+import MLP.services.api.ISegmentationService;
 import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,6 +13,7 @@ import org.springframework.stereotype.Service;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.List;
+import java.util.stream.IntStream;
 
 /**
  * author: ElinaValieva on 15.12.2018
@@ -21,29 +27,79 @@ public class TransformationService {
     private ISegmentationService segmentationService;
 
     @Autowired
-    private IResourcesService fileService;
+    private IResourcesService resourcesService;
 
     @Autowired
     private IFileManagerService fileManagerService;
 
+    @Autowired
+    private IMultiLayerPerceptronService multiLayerPerceptronService;
+
+    private static final Integer ITERATIONS = 100;
+
+    private static final Integer CNT_PATTERS = 7;
+
+    private static final Integer SIZE = 50;
+
+
     public void transform() {
-        try {
-            fileManagerService.deleteAll();
-            RImage rImage = fileService.createRImage(fileService.getFilesPath("tests_Hieroglyph/8.png"), 50, 50);
-            List<RImage> rImageList = segmentationService.segmentation(rImage);
-            rImageList.forEach(rImageItem -> {
-                try {
-                    RImage newRImage = fileService.resizeImage(rImageItem);
-                    BufferedImage bufferedImage = fileService.convertToImage(newRImage);
-                    String fileName = "file" + rImageList.indexOf(rImageItem) + ".png";
-                    System.out.println(fileName);
-                    fileManagerService.saveFile(bufferedImage, fileName);
-                } catch (IOException e) {
-                    log.error("", e);
-                }
-            });
-        } catch (IOException e) {
-            log.error("", e);
-        }
+        RImage rImage = resourcesService.createRImage(resourcesService.getFilesPath("tests_Hieroglyph/8.png"), SIZE, SIZE);
+        List<RImage> rImageList = segmentationService.segmentation(rImage);
+        training();
+        rImageList.forEach(rImageItem -> {
+            try {
+                RImage newRImage = resourcesService.resizeImage(rImageItem);
+                BufferedImage bufferedImage = resourcesService.convertToImage(newRImage);
+                String fileName = rImageList.indexOf(rImageItem) + ".png";
+                testing(newRImage);
+                fileManagerService.saveFile(bufferedImage, fileName);
+            } catch (IOException e) {
+                log.error("", e);
+            }
+        });
     }
+
+
+    public void training() {
+        final double[] error = new double[1];
+        int[] layers = new int[]{SIZE * SIZE, SIZE, CNT_PATTERS};
+        multiLayerPerceptronService.prepareLayers(layers, 0.6, new SigmoidActivation());
+        IntStream.range(0, ITERATIONS).forEach(i ->
+                IntStream.range(1, CNT_PATTERS).forEach(j -> {
+                            RImage rImagePattern = resourcesService.createRImage(resourcesService.getFilesPath("patters_Hieroglyph/" + j + ".png"), 50, 50);
+                            Double[] inputs = getList(rImagePattern.getPixels());
+
+                            if (inputs == null)
+                                System.out.println("Cant find " + j);
+
+                            Double[] output = new Double[CNT_PATTERS];
+                            for (int l = 0; l < CNT_PATTERS; l++)
+                                output[l] = 0.0;
+                            output[j - 1] = 1.0;
+                            error[0] = multiLayerPerceptronService.backPropagate(inputs, output);
+                            System.out.println("Error is " + error[0] + " (" + i + " " + " " + j + " )");
+                        }
+                )
+        );
+    }
+
+    public void testing(RImage rImage) {
+        Double[] inputs = getList(rImage.getPixels());
+        Double[] output = multiLayerPerceptronService.execute(inputs);
+        final int[] max = {0};
+        IntStream.range(0, CNT_PATTERS).forEach(i -> {
+            if (output[i] > output[max[0]])
+                max[0] = i;
+        });
+        System.out.println("Il valore massimo e' " + output[max[0]] + " pattern " + (max[0] + 1));
+    }
+
+    private Double[] getList(List<Integer> list) {
+        Double[] result = new Double[list.size()];
+        IntStream.range(0, list.size()).forEach(i ->
+                result[i] = Double.valueOf(list.get(i))
+        );
+        return result;
+    }
+
 }
