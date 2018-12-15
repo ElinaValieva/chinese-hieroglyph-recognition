ded8b4db0414da5f2ee282837957a9d197932ccb
ElinaValieva <veaufa@mail.ru>
2018-12-15 14:24:38 +0300

    refactor: delete unused classes and packages

---
+++ src/test/java/MLP/HieroglyphRecognitionApplicationTest.java
@@ -0,0 +1,18 @@
+package MLP;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * author: ElinaValieva on 15.12.2018
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HieroglyphRecognitionApplicationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+}
