15e9cc3caa9c9ca91a5fdc3839ddd57628a211b8
ElinaValieva <veaufa@mail.ru>
2018-12-22 14:19:37 +0300

    chore: upload request

--- src/main/java/MLP/controllers/MLPRestController.java
+++ src/main/java/MLP/controllers/MLPRestController.java
@@ -2,10 +2,17 @@ package MLP.controllers;
 
 import MLP.services.TransformationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
-import static MLP.configs.URL.URL_CONVERT;
+import java.io.IOException;
+
+import static MLP.configs.URL.FILE;
+import static MLP.configs.URL.URL_UPLOAD;
 
 /**
  * author: ElinaValieva on 15.12.2018
@@ -16,8 +23,10 @@ public class MLPRestController {
     @Autowired
     private TransformationService transformationService;
 
-    @GetMapping(URL_CONVERT)
-    public void segment(){
-        transformationService.transform();
+
+    @PostMapping(value = URL_UPLOAD, produces = MediaType.APPLICATION_JSON_VALUE)
+    public HttpStatus uploadFileMulti(@RequestParam(FILE) MultipartFile multipartFile) throws IOException {
+        transformationService.transform(multipartFile);
+        return HttpStatus.OK;
     }
 }
