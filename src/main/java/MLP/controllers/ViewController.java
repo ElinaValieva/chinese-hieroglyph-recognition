e405cfc3f6bf079986700b6dbb809b50f0abeceb
ElinaValieva <veaufa@mail.ru>
2018-12-22 12:19:19 +0300

    fix: view controller for start page

--- src/main/java/MLP/controllers/ViewController.java
+++ src/main/java/MLP/controllers/ViewController.java
@@ -1,7 +1,16 @@
 package MLP.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * author: ElinaValieva on 15.12.2018
  */
+@Controller
 public class ViewController {
+
+    @RequestMapping("/")
+    public String getStartPage() {
+        return "start";
+    }
 }
