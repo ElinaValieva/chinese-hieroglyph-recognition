c929d4e8b19aecc24143f790ba3bbd8bb87643d5
ElinaValieva <veaufa@mail.ru>
2018-12-23 11:43:47 +0300

    fix: ImageSegmentation result definition

--- src/main/java/MLP/models/ImageSegmentation.java
+++ src/main/java/MLP/models/ImageSegmentation.java
@@ -1,5 +1,6 @@
 package MLP.models;
 
+import MLP.configs.DictionaryConfig;
 import lombok.Data;
 
 /**
@@ -11,4 +12,9 @@ public class ImageSegmentation {
     private Integer codeResult;
     private Double error;
     private String result;
+
+    public void setCodeResult(Integer codeResult) {
+        this.codeResult = codeResult;
+        this.result = DictionaryConfig.getDictionaryConfig().getResult(codeResult);
+    }
 }
