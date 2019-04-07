0109a97c88e922d934c1a2b73d3db42ddf80b1b2
ElinaValieva <veaufa@mail.ru>
2019-04-07 15:36:19 +0300

    [Keras] Fix: neural-network model

--- recognition/HieroglyphNNModel.py
+++ recognition/HieroglyphNNModel.py
@@ -1,49 +1,41 @@
 import numpy
-from keras.datasets import mnist
 from keras.models import Sequential
-from keras.layers import Dense
-from keras.utils import np_utils
+from keras.layers import Dense, Conv2D, Flatten
+from keras.datasets import mnist
+from keras.utils import to_categorical
+from keras import backend as K
 
-# Устанавливаем seed для повторяемости результатов
+K.set_image_dim_ordering('th')
+
+# Configure seeds for returned resuls
 numpy.random.seed(42)
 
-# Загружаем данные
+# Loading data
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
-# Преобразование размерности изображений
-X_train = X_train.reshape(60000, 784)
-X_test = X_test.reshape(10000, 784)
-# Нормализация данных
-X_train = X_train.astype('float32')
-X_test = X_test.astype('float32')
-X_train /= 255
-X_test /= 255
+# Reshape data to fit model
+X_train = X_train.reshape(60000, 1, 28, 28)
+X_test = X_test.reshape(10000, 1, 28, 28)
 
-# Преобразуем метки в категории
-Y_train = np_utils.to_categorical(y_train, 10)
-Y_test = np_utils.to_categorical(y_test, 10)
+# Set results to categorical
+y_train = to_categorical(y_train)
+y_test = to_categorical(y_test)
 
-# Создаем последовательную модель
+# Configure model for neural network
 model = Sequential()
+model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(1, 28, 28), data_format='channels_first'))
+model.add(Conv2D(32, kernel_size=3, activation='relu'))
+model.add(Flatten())
+model.add(Dense(10, activation='softmax'))
 
-# Добавляем уровни сети
-model.add(Dense(800, input_shape=(784,), activation="relu", kernel_initializer="normal"))
-model.add(Dense(10, activation="softmax", kernel_initializer="normal"))
+# Compile model using accuracy as a measure of model performance
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
-# Компилируем модель
-model.compile(loss="categorical_crossentropy", optimizer="SGD", metrics=["accuracy"])
+# Train model
+model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=2)
 
-print(model.summary())
-
-# Обучаем сеть
-model.fit(X_train, Y_train, batch_size=200, epochs=25, validation_split=0.2, verbose=2)
-
-# Оцениваем качество обучения сети на тестовых данных
-scores = model.evaluate(X_test, Y_test, verbose=0)
-print("Точность работы на тестовых данных: %.2f%%" % (scores[1] * 100))
-
+# Saving model to json file
 model_json = model.to_json()
-# Записываем модель в файл
 json_file = open("mnist_model.json", "w")
 json_file.write(model_json)
 json_file.close()
