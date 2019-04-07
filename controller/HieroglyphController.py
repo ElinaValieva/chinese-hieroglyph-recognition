d4240a2276369d6bde6601ec600e101ccb7e55d4
ElinaValieva <veaufa@mail.ru>
2019-04-07 16:14:25 +0300

    [Flask] Fix: end-point method

--- controller/HieroglyphController.py
+++ controller/HieroglyphController.py
@@ -1,10 +1,16 @@
 from flask import Flask
+from flask import request
+import recognition.HieroglyphRecognition as recognitionModule
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route('/', methods=["POST"])
 def hello():
-    return "Hello World!"
+    if request.method == 'POST':
+        image_path = request.json['path']
+        result = recognitionModule.recognize(image_path)
+        return str(result)
 
 
 if __name__ == '__main__':
