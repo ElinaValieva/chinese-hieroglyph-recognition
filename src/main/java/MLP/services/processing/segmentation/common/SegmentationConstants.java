1569663e0b597d63b9ee4ed19ed76dbee8d8222a
ElinaValieva <veaufa@mail.ru>
2019-04-07 20:13:46 +0300

    [Processing] Fix: set resizing

--- src/main/java/MLP/services/processing/segmentation/common/SegmentationConstants.java
+++ src/main/java/MLP/services/processing/segmentation/common/SegmentationConstants.java
@@ -14,8 +14,8 @@ public class SegmentationConstants {
 
     public static final Color COLOR_SEGMENTS = Color.BLUE;
 
-    public static final int MATRIX_ROW_SIZE_X = 10;
-    public static final int MATRIX_ROW_SIZE_Y = 10;
+    public static final int MATRIX_ROW_SIZE_X = 1;
+    public static final int MATRIX_ROW_SIZE_Y = 1;
     public static final int THRESHOLD = 127;
 
     public static final String SEGMENTATION_RESULT_FILE_NAME = "segmentation.png";
