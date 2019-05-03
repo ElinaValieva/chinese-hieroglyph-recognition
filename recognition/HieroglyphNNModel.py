d3f61a0ba1f2305889da23e077ee21f24feb641c
ElinaValieva <veaufa@mail.ru>
2019-05-03 20:36:00 +0300

    [Keras] Fix: change model architecture

--- recognition/HieroglyphNNModel.py
+++ recognition/HieroglyphNNModel.py
@@ -1,30 +1,38 @@
+from keras.layers import Dense, Conv2D, Flatten, MaxPooling2D, Dropout
 from keras.models import Sequential
-from keras.layers import Dense, Conv2D, Flatten
+
 from util import Util
 
-count_epochs = 100
+count_epochs = 50
 image_size = 100
 
 
 # Module for creating neural network and training on dataset from 'train_data/..'
 
 def train():
-    train_x = Util.generate_dataset('../train_data/', image_size)
+    train_x = Util.generate_dataset('train', image_size)
     count_categories = Util.get_categories_cnt()
     train_y = Util.generate_categories()
 
     model = Sequential()
-    model.add(
-        Conv2D(128, (3, 3), activation='relu', input_shape=(1, image_size, image_size), data_format='channels_first'))
-    model.add(Conv2D(64, kernel_size=3, activation='relu'))
+    model.add(Conv2D(75, kernel_size=(5, 5),
+                     activation='relu',
+                     input_shape=(1, image_size, image_size)))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.2))
+    model.add(Conv2D(100, (5, 5), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.2))
     model.add(Flatten())
+    model.add(Dense(500, activation='relu'))
+    model.add(Dropout(0.5))
     model.add(Dense(count_categories, activation='softmax'))
 
-    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-
+    model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
+    print(model.summary())
     model.fit(train_x, train_y,
-              batch_size=count_categories,
-              nb_epoch=count_epochs,
+              batch_size=1,
+              epochs=count_epochs,
               verbose=1)
 
     Util.save_nn_model(model)
